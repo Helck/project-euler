558031ba1e2237aae094a4413ddb839bd8089975
ryou <ryou@toydea.com>
2017-12-11 14:33:03 +0900

    prime moduleを使うの更新

--- euler010.rb
+++ euler010.rb
@@ -1,23 +1,10 @@
 # coding: utf-8
 # Problem 10 「素数の和」
+require 'prime'
 def euler010
     sum = 0
-    (2..2000000).each do |x|
-        sum += x if prime_numbers(x)
+    Prime.each(2000000) do |x|
+        sum += x
     end
     sum
-end
-
-def prime_numbers(num)
-    return true if num == 2
-    return false if num % 2 == 0
-    x = (num ** 0.5).to_i
-    i = 3
-    while i <= x do
-        if num % i == 0
-            return false
-        end
-        i += 2
-    end 
-    return true
 end
